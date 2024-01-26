aee310aaa61637d9ca7bf6e2982c12c3a902b202
Raminder Singh <romi_ssk@yahoo.co.in>
2024-01-26 19:24:13 +0530

    use session objects

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,50 @@ use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast;
 
+trait Session {
+    fn bytes_copied(&mut self, bytes: &[u8]);
+    fn connection_closed(&self);
+    fn cancel_requested(&self);
+}
+
+struct ClientToUpstreamSession {
+    total_bytes_copied: u64,
+}
+
+impl Session for ClientToUpstreamSession {
+    fn bytes_copied(&mut self, bytes: &[u8]) {
+        self.total_bytes_copied += bytes.len() as u64;
+        eprintln!("Copied {} bytes from client to upstream", bytes.len());
+    }
+
+    fn connection_closed(&self) {
+        eprintln!("Client closed the connection");
+    }
+
+    fn cancel_requested(&self) {
+        eprintln!("Closing connection from client");
+    }
+}
+
+struct UpstreamToClientSession {
+    total_bytes_copied: u64,
+}
+
+impl Session for UpstreamToClientSession {
+    fn bytes_copied(&mut self, bytes: &[u8]) {
+        self.total_bytes_copied += bytes.len() as u64;
+        eprintln!("Copied {} bytes from upstream to client", bytes.len());
+    }
+
+    fn connection_closed(&self) {
+        eprintln!("Upstream closed the connection");
+    }
+
+    fn cancel_requested(&self) {
+        eprintln!("Closing connection to upstream");
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:8080";
@@ -28,41 +72,51 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let (cancel_sender, _) = broadcast::channel::<()>(1);
 
-            let (client_copied, upstream_copied) = tokio::join! {
-                copy_bytes(&mut client_reader, &mut upstream_writer, "client", "upstream", cancel_sender.subscribe())
+            let mut client_to_upstream_session = ClientToUpstreamSession {
+                total_bytes_copied: 0,
+            };
+            let mut upstream_to_client_session = UpstreamToClientSession {
+                total_bytes_copied: 0,
+            };
+
+            let (client_res, upstream_res) = tokio::join! {
+                copy_bytes(&mut client_reader, &mut upstream_writer,
+                        &mut client_to_upstream_session, cancel_sender.subscribe())
                     .then(|r| { let _ = cancel_sender.send(()); async { r } }),
-                copy_bytes(&mut upstream_reader, &mut client_writer, "upstream", "client", cancel_sender.subscribe())
+                copy_bytes(&mut upstream_reader, &mut client_writer,
+                        &mut upstream_to_client_session, cancel_sender.subscribe())
                     .then(|r| { let _ = cancel_sender.send(()); async { r } }),
             };
 
-            match client_copied {
-                Ok(count) => {
-                    eprintln!("Copied total {count} bytes from client to upstream in this session")
-                }
-                Err(e) => eprintln!("Failed to copy bytes from client to upstream; err = {e:?}"),
+            eprintln!(
+                "Copied total {} bytes from client to upstream in this session",
+                client_to_upstream_session.total_bytes_copied
+            );
+            eprintln!(
+                "Copied total {} bytes from upstream to client in this session",
+                upstream_to_client_session.total_bytes_copied
+            );
+            if let Err(e) = client_res {
+                eprintln!("Error while copying bytes from client to upstream; err = {e:?}");
             }
-            match upstream_copied {
-                Ok(count) => {
-                    eprintln!("Copied total {count} bytes from upstream to client in this session")
-                }
-                Err(e) => eprintln!("Failed to copy bytes from upstream to client; err = {e:?}"),
+            if let Err(e) = upstream_res {
+                eprintln!("Error while copying bytes from upstream to client; err = {e:?}");
             }
         });
     }
 }
 
-async fn copy_bytes<R, W>(
+async fn copy_bytes<R, W, S>(
     reader: &mut R,
     writer: &mut W,
-    source_name: &str,
-    dest_name: &str,
+    session: &mut S,
     mut cancel_receiver: broadcast::Receiver<()>,
-) -> tokio::io::Result<usize>
+) -> tokio::io::Result<()>
 where
     R: AsyncRead + Unpin,
     W: AsyncWrite + Unpin,
+    S: Session,
 {
-    let mut bytes_copied = 0;
     let mut buf = [0; 1024];
     loop {
         let bytes_read;
@@ -73,21 +127,20 @@ where
                 bytes_read = result?;
             }
             _ = cancel_receiver.recv() => {
-                eprintln!("Closing connection to {source_name}");
+                session.cancel_requested();
                 break;
             }
         }
 
         // normal connection close
         if bytes_read == 0 {
-            eprintln!("Connection from {source_name} closed");
+            session.connection_closed();
             break;
         }
 
         writer.write_all(&buf[0..bytes_read]).await?;
-        eprintln!("Copied {bytes_read} bytes from {source_name} to {dest_name}");
-        bytes_copied += bytes_read;
+        session.bytes_copied(&buf[0..bytes_read]);
     }
 
-    Ok(bytes_copied)
+    Ok(())
 }
